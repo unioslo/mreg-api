dd4d34e2f58dd6d67471379739f7ccd4743d32d4
pederhan <pederhan@usit.uio.no>
2026-01-14 15:30:22 +0100

    Update `__all__`

--- mreg_api/__init__.py
+++ mreg_api/__init__.py
@@ -2,10 +2,20 @@
 
 from __future__ import annotations
 
+from mreg_api import cache
+from mreg_api import dirs
+from mreg_api import endpoints
+from mreg_api import exceptions
+from mreg_api import types
 from mreg_api.__about__ import __version__
 from mreg_api.client import MregClient
 
 __all__ = [
     "__version__",
     "MregClient",
+    "cache",
+    "endpoints",
+    "exceptions",
+    "dirs",
+    "types",
 ]
