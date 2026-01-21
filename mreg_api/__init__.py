79fd7462f28ec285ae7c732d83d977b3626b7b2a
pederhan <pederhan@usit.uio.no>
2026-01-21 14:01:40 +0100

    Update module exports

--- mreg_api/__init__.py
+++ mreg_api/__init__.py
@@ -2,16 +2,20 @@
 
 from __future__ import annotations
 
+from mreg_api import cache
 from mreg_api import dirs
 from mreg_api import endpoints
 from mreg_api import exceptions
 from mreg_api import types
 from mreg_api.__about__ import __version__
+from mreg_api.cache import CacheConfig
 from mreg_api.client import MregClient
 
 __all__ = [
     "__version__",
     "MregClient",
+    "CacheConfig",
+    "cache",
     "endpoints",
     "exceptions",
     "dirs",
