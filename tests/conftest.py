6555dc966d592d0ee7e34ab426cbd45140ca4b55
pederhan <pederhan@usit.uio.no>
2026-01-27 12:31:34 +0100

    Trigger _reset_contextvars in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,8 @@ def reset_instance_after_test() -> Generator[None, None, None]:
         yield
     finally:
         try:
+            # FIXME: Can we force the destructor to be called?
+            MregClient()._reset_contextvars()  # pyright: ignore[reportPrivateUsage]
             MregClient.reset_instance()
         except KeyError:
             pass
