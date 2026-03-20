d4d33f45cd635d8f39a59508c1629619cf505033
pederhan <pederhan@usit.uio.no>
2026-03-20 17:52:34 +0100

    Fix import

--- mreg_api/events.py
+++ mreg_api/events.py
@@ -7,7 +7,8 @@ from dataclasses import dataclass
 from dataclasses import field
 from datetime import datetime
 from enum import Enum
-from typing import override
+
+from typing_extensions import override
 
 
 class EventKind(str, Enum):
